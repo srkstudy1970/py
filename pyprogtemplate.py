4708a9c47762e80837d36639fe2836cb4b67cd61
srkstudy1970 <srkstudy1970@users.noreply.github.com>
2016-08-29 09:55:34 +0800

    Add files via upload

---
+++ pyprogtemplate.py
@@ -0,0 +1,24 @@
+#######################- About this Program-##############
+# This is for text analysis
+#1. Packages
+#2. Functions
+#3. Welcome Menu
+#3. Main 
+#######################- END -About this Program-##############
+
+#######################- Packages-##############
+print "Packages"
+
+#######################- End of Packages-##############
+
+#######################- Functions-##############
+print "Functions"
+#######################- End of Functions-##############
+
+#######################- Welcome Menu-##############
+print "Menu"
+#######################- End of Welcomg Menu-##############
+
+#######################- Main-##############
+print "Main"
+#######################- End of Main-##############
